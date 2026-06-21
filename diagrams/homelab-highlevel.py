57b9f4b4d6d23e1e232417d5dea4e0afec7e87fc
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2026-06-21 09:52:15 +0930

    add cloudflare tunnel

--- diagrams/homelab-highlevel.py
+++ diagrams/homelab-highlevel.py
@@ -12,50 +12,65 @@ with Diagram(
     direction="TB",
 ):
     router = Router("Router")
+    tunnel_access = Custom(
+        "Cloudflare tunnel", "./custom-icons/cloudflare-zero-trust.png"
+    )
+    user = Custom("User", "./custom-icons/user.png")
 
     with Cluster("HomeLab"):
-        admin = Custom("Admin", "./custom-icons/admin.png")
-        user = Custom("User", "./custom-icons/user.png")
         switch = Switch("5 Port Switch")
+        admin = Custom("Admin", "./custom-icons/admin.png")
+        # Piping
         router >> switch
+
         with Cluster("The Butler"):
             server_machine = Custom("The Butler", "./custom-icons/fedora.png")
             cockpit = Custom("Cockpit", "./custom-icons/cockpit.png")
+            # Piping
             switch >> server_machine
-            server_machine - cockpit
-            cockpit >> admin
+            admin << cockpit
+
             with Cluster("Docker Services"):
-                docker_directory = Docker("/srv/homelab/docker-services")
+                cloudflare_tunnel = Custom(
+                    "cloudflare tunnel", "./custom-icons/cloudflare.png"
+                )
+                # Piping
+                cloudflare_tunnel << tunnel_access << user
+                docker_directory = Docker("/srv/homelab/\ndocker-services")
                 server_machine >> docker_directory
+
                 with Cluster("Media"):
                     media_directory = Folder("Media Directory")
-                    with Cluster("Jellyfin"):
+                    with Cluster("Jellyfin stack"):
                         jellyfin = Custom("Jellyfin", "./custom-icons/jellyfin.png")
                         jellyseer = Custom("Jellyseer", "./custom-icons/jellyseerr.png")
-                    with Cluster("automation"):
+                    with Cluster("automation stack"):
                         qbittorent = Custom(
                             "qBittorent", "./custom-icons/qbittorrent.png"
                         )
                         sonarr = Custom("Sonarr", "./custom-icons/sonarr.png")
                         radarr = Custom("Radarr", "./custom-icons/radarr.png")
-
-                    jellyfin >> user
+                    # Piping
+                    jellyfin >> cloudflare_tunnel
                     jellyfin >> media_directory
                     qbittorent >> media_directory
                     jellyseer >> radarr
                     jellyseer >> sonarr
+
                 with Cluster("AI LLM"):
 
-                    with Cluster("Open Web UI"):
+                    with Cluster("Open Web UI stack"):
                         open_web_ui = Custom(
                             "Open Web UI", "./custom-icons/open-webui.png"
                         )
                         ollama = Custom("Ollama", "./custom-icons/ollama.png")
-                    with Cluster("Lite LLM"):
+                    with Cluster("Lite LLM stack"):
                         lite_LLM = Custom("Lite LLM", "./custom-icons/litellm.png")
-                    open_web_ui >> user
+                    # Piping
+                    open_web_ui >> cloudflare_tunnel
                     lite_LLM >> open_web_ui
 
-                with Cluster("Portainer"):
+                with Cluster("Portainer stack"):
                     portainer = Custom("Portainer", "./custom-icons/portainer-v1.png")
-                    portainer >> admin
+                    # Piping
+                    portainer << admin
